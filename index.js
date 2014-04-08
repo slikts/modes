026deeeaaf405806abd634734c6092d071a67a14
slikts <dabas@untu.ms>
2014-04-08 20:24:42 +0300

    upd

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 
 var argv = require('minimist')(process.argv.slice(2));
 
+var pkginfo = require('./package');
+
 var app = require('./lib/app');
+
+app.set('version', pkginfo.version);
+app.set('name', pkginfo.name);
 
 if (argv.adduser) {
   app.addUser({
